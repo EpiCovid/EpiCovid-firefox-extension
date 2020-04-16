3e38b9ed8d2723c8446cbbf7825d5db211715620
Kylian <kylian.cadet@epitech.eu>
2020-04-16 15:53:16 +0200

    clean code, add documentation as comments 	modified:   popup/epi-covid.js

--- popup/epi-covid.js
+++ popup/epi-covid.js
@@ -1,17 +1,42 @@
+// Get all main elements by ID
 const refresh_btn = document.querySelector("#refresh-btn")
 const main_content = document.querySelector("#main-content")
 const loader = document.querySelector("#loader")
+
+// Set global variable
 var error = false;
 var last_update = 0;
 var confirmed = 0;
 var death = 0;
 var recovered = 0;
 
-
+// Add listener on the refresh button
 const debouncedUpdateData = debounce(updateData, 500, null);
 refresh_btn.addEventListener("click", debouncedUpdateData);
 
-function fillContent(data) {
+// When first open, update the data
+window.onfocus = updateData
+
+/**
+ * This function return a div with the selected class names and data
+ * @param {String} class_name
+ * @param {Number} data
+ */
+function createContent(class_name, data) {
+  var div = document.createElement("div")
+  div.setAttribute("class", class_name)
+  var span = document.createElement("span")
+  span.textContent = data
+
+  div.appendChild(span)
+  return div
+}
+
+/**
+ * Fills global variable "confirmed", "last_update", "death" and "recovered"
+ * @param {Array} data 
+ */
+function fillData(data) {
   last_update = data[0]["attributes"]["Last_Update"];
   last_update = new Date(last_update).toLocaleTimeString()
   confirmed = 0;
@@ -22,29 +47,18 @@ function fillContent(data) {
     recovered += data[i]["attributes"]["Recovered"]
     death += data[i]["attributes"]["Deaths"]
   }
-  var last_update_content = document.createElement("div")
-  last_update_content.setAttribute("class", "content last_update")
-  var last_update_text = document.createElement("p")
-  last_update_text.textContent = "last update : " + last_update
-  last_update_content.appendChild(last_update_text)
+}
 
-  var confirmed_content = document.createElement("div")
-  confirmed_content.setAttribute("class", "content confirmed")
-  var confirmed_text = document.createElement("p")
-  confirmed_text.textContent = "confirmed : " + confirmed
-  confirmed_content.appendChild(confirmed_text)
-
-  var death_content = document.createElement("div")
-  death_content.setAttribute("class", "content death")
-  var death_text = document.createElement("p")
-  death_text.textContent = "death : " + death
-  death_content.appendChild(death_text)
-
-  var recovered_content = document.createElement("div")
-  recovered_content.setAttribute("class", "content recovered")
-  var recovered_text = document.createElement("p")
-  recovered_text.textContent = "recovered : " + recovered
-  recovered_content.appendChild(recovered_text)
+/**
+ * Inject data contents into the html popup, and supress the previous one
+ * @param {Array} data 
+ */
+function fillContent(data) {
+  fillData(data)
+  var last_update_content = createContent("content last_update", last_update)
+  var confirmed_content = createContent("content confirmed", confirmed)
+  var death_content = createContent("content death", death)
+  var recovered_content = createContent("content recovered", recovered)
 
   var contents = document.querySelectorAll(".content")
   for (var i = 0; i != contents.length; i++)
@@ -56,12 +70,12 @@ function fillContent(data) {
   main_content.appendChild(death_content)
 }
 
-window.onfocus = start
-
-function start() {
-  updateData();
-}
-
+/**
+ * Return the debounded function passed as parameter
+ * @param {Function} func
+ * @param {Number} wait
+ * @param {Any} immediate
+ */
 function debounce(func, wait, immediate) {
   var timeout;
   return function () {
@@ -77,7 +91,11 @@ function debounce(func, wait, immediate) {
   }
 }
 
+/**
+ * Fetch arcgis api, retrieve required data and call fillContent()
+ */
 function updateData() {
+  error = false
   loader.setAttribute("class", "loading")
   fetch("https://services1.arcgis.com//0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=Confirmed+%3E+0&returnGeometry=false&outFields=Confirmed%2C+Last_Update%2C+Recovered%2C+Deaths&orderByFields=Last_Update+desc&resultRecordCount=1000")
     .then((reponse) => reponse.json())
